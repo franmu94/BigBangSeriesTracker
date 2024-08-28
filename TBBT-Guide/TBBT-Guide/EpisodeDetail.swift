6be76da1f50208e371e0fe3f0da75461ba10d955
franmu94 <fmalourarte@gmail.com>
2024-08-28 23:16:33 +0200

    detalle episode

---
+++ TBBT-Guide/TBBT-Guide/EpisodeDetail.swift
@@ -0,0 +1,18 @@
+//
+//  EpisodeDeatil.swift
+//  TBBT-Guide
+//
+//  Created by Fran Malo on 28/8/24.
+//
+
+import SwiftUI
+
+struct EpisodeDeatil: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EpisodeDeatil()
+}
